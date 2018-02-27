cd87f46d2e70325f319296033a5c5f3303113c1f
Tinram <Tinram@users.noreply.github.com>
2018-02-27 22:46:47 +0000

    add OpenSSL-File-Encrypt files

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# OpenSSL File Encrypt
+
+#### Simple OpenSSL symmetric file encryption.
+
+
+## Purpose
+
+Provide simple and strong and file encryption using OpenSSL, via PHP.
+
+One use is to avoid direct OpenSSL interaction, such as:
+
+        openssl enc -e -aes-256-cbc -in abc.txt -out abc.enc -k password -S deadbeef
+
+and offer something simpler:
+
+        php cmdline_example.php -e abc.txt
+
+
+## License
+
+OpenSSL File Encrypt is released under the [GPL v.3](https://www.gnu.org/licenses/gpl-3.0.html).
---
+++ cmdline_example.php
@@ -0,0 +1,118 @@
+#!/usr/bin/env php
+<?php
+
+/**
+    * Command-line example usage of OpenSSL-File-Encrypt class.
+    *
+    * Processes up to 1.8 GB on CLI, subject to memory availability.
+    *
+    * Usage:
+    *        php <thisfilename> -e|-d <filename>
+    *
+    * @author        Martin Latter <copysense.co.uk>
+    * @copyright     Martin Latter 20/02/2018
+    * @version       0.05
+    * @license       GNU GPL v3.0
+    * @link          https://github.com/Tinram/OpenSSL-File-Encrypt.git
+*/
+
+
+declare(strict_types=1);
+
+
+define('DUB_EOL', PHP_EOL . PHP_EOL);
+define('LINUX', (stripos(php_uname(), 'linux') !== FALSE) ? TRUE : FALSE);
+
+
+if ( ! extension_loaded('openssl'))
+{
+    die(PHP_EOL . ' OpenSSL library not available!' . DUB_EOL);
+}
+
+
+require('classes/openssl_file.class.php');
+
+
+$sUsage =
+    PHP_EOL . ' ' . basename(__FILE__, '.php') .
+    DUB_EOL . "\tusage: php " . basename(__FILE__) . ' -e|-d <file> ' . ( ! LINUX ? '<password>' : '') . DUB_EOL;
+
+$sMode = null;
+$aOptions = getopt('h::e::d::', ['help::', 'h::']);
+
+if ( ! empty($aOptions))
+{
+    $sOpt = key($aOptions);
+
+    switch ($sOpt)
+    {
+        case 'h':
+            die($sUsage);
+        break;
+
+        case 'e':
+        case 'd':
+           $sMode = $sOpt;
+        break;
+    }
+}
+else
+{
+    die($sUsage);
+}
+
+if ( ! isset($_SERVER['argv'][2]))
+{
+    echo PHP_EOL . ' missing filename!' . PHP_EOL;
+    die($sUsage);
+}
+
+$sFilename = $_SERVER['argv'][2];
+
+if ( ! file_exists($sFilename))
+{
+    die(PHP_EOL . ' \'' . $sFilename . '\' does not exist in this directory!' . DUB_EOL);
+}
+
+
+if (LINUX)
+{
+    echo ' password: ';
+    `/bin/stty -echo`;
+    $sPassword = trim(fgets(STDIN));
+    `/bin/stty echo`;
+
+    if ($sMode === 'e')
+    {
+        echo PHP_EOL . ' re-enter password: ';
+        `/bin/stty -echo`;
+        $sPassword2 = trim(fgets(STDIN));
+        `/bin/stty echo`;
+
+        if ($sPassword !== $sPassword2)
+        {
+            die(PHP_EOL . ' entered passwords do not match!' . DUB_EOL);
+        }
+    }
+}
+else
+{
+    if ( ! isset($_SERVER['argv'][3]))
+    {
+        die(PHP_EOL . ' missing password!' . DUB_EOL . "\tusage: " . basename(__FILE__) . ' -e|-d <file> <password>' . DUB_EOL);
+    }
+    else
+    {
+        $sPassword = $_SERVER['argv'][3];
+    }
+}
+
+
+if ($sMode === 'e')
+{
+    echo OpenSSLFile::encrypt($sFilename, $sPassword);
+}
+else if ($sMode === 'd')
+{
+    echo OpenSSLFile::decrypt($sFilename, $sPassword);
+}
